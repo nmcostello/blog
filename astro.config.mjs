64108bf828d87c048bdc106a6f8563c4811db202
Noah Costello <29776732+nmcostello@users.noreply.github.com>
2026-02-13 08:00:22 -0500

    configure for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://blog-floral-hill-3820.fly.dev',
+  site: 'https://nmcostello.github.io/blog',
+  outDir: 'docs',
   output: 'static'
 });
