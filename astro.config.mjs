9fe36710747de01f45ff9d68c4c9db38030d40ad
Noah Costello <29776732+nmcostello@users.noreply.github.com>
2026-02-13 16:03:52 -0500

    fix: update base path for github pages subpath

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://nmcostello.github.io',
-  base: '/',
+  site: 'https://nmcostello.github.io/blog',
+  base: '/blog/',
   outDir: 'docs',
   output: 'static'
 });
